8f1e97c79a50f5dd64692b1d02c8742ae7972573
Jean THOMAS <contact@tibounise.com>
2015-11-14 16:41:29 +0100

    Setting the version number to 1.0.0

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@
 from setuptools import setup
 
 setup(name='pygments-algo-lexer',
-      version='0.0.1',
+      version='1.0.0',
       description='Pygments algo lexer.',
       keywords='pygments algo lexer',
       license='BSD',
