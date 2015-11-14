23dc4549518c4ea7533635d61f0ea695e30de016
Jean THOMAS <contact@tibounise.com>
2015-11-14 12:24:24 +0100

    First working version

--- pygments_algo_lexer/algo.py
+++ pygments_algo_lexer/algo.py
@@ -28,18 +28,45 @@ class AlgoLexer(RegexLexer):
 
     flags = re.IGNORECASE | re.DOTALL
 
+    stringIdentifierRegex = r'[\xC0-\xD6\xD8-\xF6\xF8-\xFFa-zA-Z_][\xC0-\xD6\xD8-\xF6\xF8-\xFFa-zA-Z0-9_]*'
+
     tokens = {
         'root': [
+            include('headers'),
+            include('comment'),
+            include('data'),
+            include('ponctuation'),
+            include('keywords'),
+            include('functions'),
+            include('operators'),
             include('whitespace'),
-            (r'(algorithme)(\s*)([^\s]+)',bygroups(Keyword, Text, Name)),
-            (r'\(\*', Comment.Multiline, 'comment'),
-            (r'(fonction)(\s*)\(([^\s]+)\):([^\s]+)',bygroups(Keyword, Text, Name.Function, Keyword.Type)),
-            (r'entier|réel|chaîne|car|booléen',Keyword.Type),
-            (r'var',Keyword.Declaration),
-            (r'([^\s]+)(\s*)(:)',bygroups(Name.Variable, Text, Operator)),
-            (r'début',Keyword.Reserved,'block'),
+            include('variables'),
+        ],
+        'ponctuation': [
+            (r'(,|;|\(|\))',Punctuation)
+        ],
+        'headers': [
+            (r'(algorithme)(\s*)('+stringIdentifierRegex+')',bygroups(Keyword, Text, Name)),
+            (r'(fonction)(\s*)('+stringIdentifierRegex+')',bygroups(Keyword, Text, Name.Function)),
+            (r'(proc\xE9dure)(\s*)('+stringIdentifierRegex+')',bygroups(Keyword, Text, Name.Function)),
+        ],
+        'keywords': [
+            (r'\bd\xE9but|sinon|fin|tant que|faire|selon|pour|de|\xE0|si|alors\b', Keyword.Reserved),
+            (r'\bin|ex|entier|r\xE9el|chaÃ®ne|car|bool\xE9en\b',Keyword.Type),
+            (r'\bvar\b',Keyword.Declaration),
+            (r'\bfdl\b',Keyword.Constant)
+        ],
+        'functions': [
+            (r'('+stringIdentifierRegex+')(\s*)(\()', bygroups(Name.Function,Text,Punctuation))
+        ],
+        'variables': [
+            (stringIdentifierRegex, Name.Variable),
+        ],
+        'data': [
             (r'"',String.Double, 'string'),
-            (r',',Punctuation)
+            (r'(\')([^\'])(\')',bygroups(String.Simple, Text, String.Simple)),
+            (r'(\d\.\d)',Number.Float),
+            (r'\d',Number.Integer),
         ],
         'whitespace': [
             (r'\n', Text),
@@ -51,13 +78,18 @@ class AlgoLexer(RegexLexer):
             (r'"', String.Double, '#pop'),
         ],
         'comment': [
+            (r'\(\*', Comment.Multiline, 'commentblock'),
+            (r'--([^\n]*)', Comment.Single)
+        ],
+        'commentblock': [
             (r'[^*\)]', Comment.Multiline),
             (r'\(\*', Comment.Multiline, '#push'),
             (r'\*\)', Comment.Multiline, '#pop'),
             (r'[\*\)]', Comment.Multiline)
         ],
-        'block':[
-            (r'fin',Keyword.Reserved,'#pop')
+        'operators':[
+            (r'(<-|/=|=|>|<|:|\+|-|/)',Operator),
+            (r'(et|ou|non|div|mod)',Operator.Word)
         ]
     }
 
